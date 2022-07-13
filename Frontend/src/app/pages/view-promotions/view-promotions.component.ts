9ea0594fa0ea8bea83435ba4baa8f633f991f208
valruja <vrujana@excelsiorgama.com>
2022-07-13 19:47:08 -0400

    una opcion de datatable

--- Frontend/src/app/pages/view-promotions/view-promotions.component.ts
+++ Frontend/src/app/pages/view-promotions/view-promotions.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-promotions',
   templateUrl: './view-promotions.component.html',
-  styleUrls: ['./view-promotions.component.scss']
+  styleUrls: ['./prueba.css']
 })
 export class ViewPromotionsComponent implements OnInit {
 
